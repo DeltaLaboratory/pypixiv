9d1cc47c1f997679afd5a388ead2bb0b2aa42ad2
DeltaLaboratory <delta@deltalab.dev>
2022-01-16 05:07:23 +0900

    added some info in __init__.py

--- pypixiv/__init__.py
+++ pypixiv/__init__.py
@@ -1,3 +1,10 @@
+__package__ = "pypixiv"
+__doc__ = "pixiv api wrapper library"
+__all__ = (
+    "Client",
+    "exceptions",
+    "models"
+)
 from ._client import Client
 from . import _exceptions as exceptions
 from . import _models as models
